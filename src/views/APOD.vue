e0b4cb5d0f9a2c0b31c2d8fd4ca38eae96c3e33c
Radin Nedkov <radin@routenote.com>
2022-03-02 03:13:26 +0200

    apotd by date added

--- src/views/APOD.vue
+++ src/views/APOD.vue
@@ -1,27 +1,58 @@
 <template>
-    <div class="w-2/3 lg:w-2/4 mx-auto mt-12">
-
+    <div class="w-2/3 lg:w-2/4 mx-auto mt-8 text-center mb-12">
+        <div class="text-lg">
+            <p class="inline-block">Astronomy Picture of </p>
+            <Datepicker class="ml-1 w-1/6 inline-block" 
+            placeholder="Select Date" 
+            v-model="newDate" 
+            :format="'yyyy-MM-dd'" />
+        </div>
+        <p v-if="data.title" class="mt-8 text-base">"{{ data.title }}" by {{ data.copyright }}</p>
+        <img v-if="data.url" 
+        class="border-4 border-indigo-700 rounded-md"
+        :src="data.url" 
+        :alt="data.title">
+        <p class="mt-4 text-red-600" v-else>{{ error }}</p>
     </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 import { NASA_URL } from '../resources/constants'
+import Datepicker from "vuejs-datepicker";
+
 const APOD = Vue.extend({
     data() {
         return {
-            apiKey: 'api_key=DSkVsLRAEdMjcfO78LWBbbv8kKdH97wIEdOilQfe'
+            apiKey: 'api_key=DSkVsLRAEdMjcfO78LWBbbv8kKdH97wIEdOilQfe',
+            data: {},
+            newDate: '',
+            error: 'No results for the given date.'
         }
     },
     created() {
-        this.getPictureOfTheDay();
+        this.actualDate();
     },
     methods: {
-        getPictureOfTheDay() {
-            this.$http.get(NASA_URL + '/planetary/apod?' + this.apiKey).then((response) => {
-                console.log(response.data)
-            })
+        getPictureOfTheDay(value: any) {
+            this.$http.get(NASA_URL + '/planetary/apod?' + this.apiKey + '&date=' + this.newDate)
+            .then((response) => {
+                this.data = response.data
+            });
         },
+        actualDate() {
+            this.newDate = new Date().toISOString().slice(0, 10)
+        }
+    },
+    watch: {
+        'newDate'(newValue) {
+            let convertedDate = new Date(newValue).toISOString().slice(0, 10);
+            this.newDate = convertedDate;
+            this.getPictureOfTheDay(convertedDate);
+        }
+    },
+    components: {
+        Datepicker
     }
 });
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="mt-8 mx-auto w-2/3" v-if="!$auth.loading">
-    <h1 class="text-center">Welcome to Eden Application Task</h1>
-    <h2 class="mt-4 text-left lg:text-center">This an applicant task app, which aims to test all position relevant skills. </h2>
+    <p class="text-center text-lg">Welcome to Eden Application Task</p>
+    <p class="mt-4 text-left text-base lg:text-center">This is an applicant task app, which aims to test all position relevant skills. </p>
   </div>
 </template>
 
